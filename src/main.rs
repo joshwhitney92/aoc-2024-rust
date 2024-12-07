7c8d58af226afaa326f7cbfdb14ac38931a749ee
josh <joshwhtny@yahoo.com>
2024-12-06 23:57:53 -0800

    Day 1, solution 1 implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use solutions::day_1::Solution1;
+
+pub mod solutions;
+
+fn main() -> anyhow::Result<()> {
+    let file_path = "./problem_inputs/day_1.txt";
+    let result = Solution1::solve(file_path)?;
+
+    println!("Total is: {}", result);
+
+    Ok(())
 }
