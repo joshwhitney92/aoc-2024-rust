59fc7cfc298e29051960320739668ff9e2f2bbf9
josh <joshwhtny@yahoo.com>
2024-12-10 11:32:39 -0800

    setting up solution for day 2

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-use solutions::day_1::{Solution1, Solution2};
+// use solutions::day_1::Solution1;
+use solutions::day_1::Solution2;
 
 pub mod solutions;
 
 fn main() -> anyhow::Result<()> {
-    let file_path = "./problem_inputs/day_1.txt";
-    let result = Solution1::solve(file_path)?;
-    let result2 =  Solution2::solve(file_path)?;
+    // Day 1 solution.
+    // let file_path = "./problem_inputs/day_1.txt";
+    // let result = Solution1::solve(file_path)?;
+    // let result2 =  Solution2::solve(file_path)?;
 
-    println!("Solution 1 Total is: {}", result);
-    println!("Solution 2 Total is: {}", result2);
+    // println!("Solution 1 Total is: {}", result);
+    // println!("Solution 2 Total is: {}", result2);
     
 
     Ok(())
